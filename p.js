f0445e367b61803d9abfa00be041608691577ab0
preeti-1999 <56556771+preeti-1999@users.noreply.github.com>
2020-02-11 00:06:19 +0530

    Add files via upload

---
+++ p.js
@@ -0,0 +1,27 @@
+$(window).scroll(function() {
+ 
+    if ($(window).scrollTop() > 300) {
+        $('.main_nav').addClass('sticky');
+    } else {
+        $('.main_nav').removeClass('sticky');
+    }
+});
+
+
+
+jQuery(document).ready(function($) {
+ 
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+ 
+	    var target = this.hash,
+	    $target = $(target);
+ 
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+});
